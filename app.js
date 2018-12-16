c093de0a05655d7e0e57f42d1fd790a1191a2285
Andrew Olepir <olepir.andrew@gmail.com>
2018-12-16 12:14:06 +0200

    finish task2

--- app.js
+++ app.js
@@ -1,42 +1,124 @@
-const isNumeric = n => !isNaN(parseFloat(n)) && isFinite(n);
+//<=======================create template=========================>
 
-let answerDiv = document.createElement('div');
-answerDiv.className = 'answerContainer taskContainer';
+const getTemplateArr = (size, arr = [], counter = 0) => {
+  for (var i = 0; i < size; i++) {
+    arr[i] = [];
 
-const enterNumber = () => {
-  let currentNumber = true,
-      resultArr = [],
-      message = 'enter the number or click cancel',
-      varning = 'THIS IS NOT A NUMBER';
-
-  while (currentNumber) {
-    currentNumber = prompt(message);
-    if (currentNumber === null) {
-      break;
-    } else if (!isNumeric(currentNumber)) {
-      alert(varning);
-    } else {
-      resultArr.push(+currentNumber);
+    for (var j = 0; j < size; j++, counter++) {
+      arr[i][j] = counter;
     }
   }
-  return resultArr;
+  return arr;
 };
 
-const showResult = () => {
-  let matchMessage = '';
-  const answer = enterNumber();
+console.table(getTemplateArr(10));
 
-  matchMessage =
-    answer.length === [...new Set(answer)].length
-      ? 'no matches'
-      : 'there is matches';
+//<=======================create Bomba=========================>
 
-  answerDiv.innerHTML = `${matchMessage}  ${answer
-    .sort((a, b) => a - b)
-    .join()}`;
-
-  answer.length ? document.body.append(answerDiv) : answerDiv.remove();
+const checkBomba = arr => {
+  return arr.map(n => {
+    let res = (n % 3 ? '' : 'Super') + (n % 5 ? '' : 'Buper') || n;
+    return res === 'SuperBuper' ? (res = 'BOOM') : res;
+  });
 };
 
-const button = document.querySelector('.task-button');
-button.addEventListener('click', showResult);
+const getBombaArr = size => {
+  return getTemplateArr(size).map(n => checkBomba(n));
+};
+console.table(getBombaArr(10));
+
+//<=====================square left===========================>
+
+getPerimetrSquare = (size, rightSide = []) => {
+  return getTemplateArr(size).reduce((pre, cur, i, arr) => {
+    if (i === 0) {
+      return pre.concat(cur.reverse());
+    }
+    if (i === arr.length - 1) {
+      return pre.concat(cur, rightSide);
+    }
+    pre.push(cur[0]);
+    rightSide.unshift(cur.pop());
+    return pre;
+  }, []);
+};
+
+console.log('Square :', getPerimetrSquare(10));
+
+//<=======================square left 2=========================>
+
+getPerimetrSquare2 = size => {
+  let arr = getTemplateArr(size),
+    resultArr = arr[0].reverse(),
+    rightSide = [],
+    middle = getTemplateArr(size).slice(1, -1);
+  middle.forEach(n => {
+    resultArr.push(n[0]);
+    rightSide.unshift(n.pop());
+  });
+
+  return (resultArr = resultArr.concat(arr.pop(), rightSide));
+};
+console.log('Square2 :', getPerimetrSquare2(10));
+
+//<=====================triangle right ===========================>
+
+getPerimetrTriangle = (size, diagonal = []) => {
+  return getTemplateArr(size).reduce((pre, cur, i, arr) => {
+    if (i === 0) {
+      return pre.concat(cur);
+    }
+    if (i === arr.length - 1) {
+      return pre.concat(cur.pop(), diagonal);
+    }
+    pre.push(cur.pop());
+    diagonal.unshift(cur[i]);
+    return pre;
+  }, []);
+};
+
+console.log('Triangle :', getPerimetrTriangle(10));
+
+//<=======================triangle bottom=========================>
+
+getPerimetrTriangleBottom = (size, diagonal = []) => {
+  return getTemplateArr(size).reduceRight((pre, cur, i, arr) => {
+    if (i === 0) {
+      pre.push(cur[0]);
+      return pre.concat(
+        diagonal,
+        arr
+          .pop()
+          .slice(1, -1)
+          .reverse()
+      );
+    }
+    pre.push(cur[0]);
+    diagonal.unshift(cur[i]);
+    return pre;
+  }, []);
+};
+
+console.log('TriangleBottom :', getPerimetrTriangleBottom(10));
+
+//<=======================helix=========================>
+
+const run = (input, result, leftSideArr = []) => {
+  if (!input.length) {
+  //end recursion
+    return result;
+  }
+  //add top side
+  result = result.concat(input.shift());
+  //add right side
+  input.forEach(last => result.push(last.pop()));
+  //create left side
+  input.forEach(first => leftSideArr.unshift(first.shift()));
+  //add bottom side
+  result = input.length ? result.concat(input.pop().reverse()) : result;
+  //add leftSide
+  result = result.concat(leftSideArr);
+  //all over again
+  return run(input, result);
+};
+console.log('helix :', run(getTemplateArr(10), []));
