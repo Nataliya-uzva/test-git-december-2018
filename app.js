4f0c6909ad0460d67cbf6e2dc775dc71ded1df27
Andrew Olepir <olepir.andrew@gmail.com>
2018-12-19 16:39:28 +0200

    solution task about train

--- app.js
+++ app.js
@@ -1,121 +1,69 @@
-//<=======================create template=========================>
+const getVagonQuantity = (max = 20, min = 10) =>
+  Math.round(min - 0.5 + Math.random() * (max - min + 1));
 
-const getTemplateArr = (size, arr = [], counter = 0) => {
-  for (var i = 0; i < size; i++) {
-    arr[i] = [];
-    for (var j = 0; j < size; j++, counter++) {
-      arr[i][j] = counter;
+class Train {
+  constructor(sizeMax) {
+		this.vagons = [],
+		this.vagonCounter = 0;
+    // генерим выгоны
+    for (var i = 0; i < getVagonQuantity(sizeMax); i++) {
+      this.vagons.push({
+        lightIsOn: !!Math.round(Math.random()),
+      });
     }
   }
-  return arr;
-};
-
-console.table(getTemplateArr(10));
-
-//<=======================create Bomba=========================>
-
-const checkBomba = arr =>
-  arr.map(n => {
-    let res = (n % 3 ? '' : 'Super') + (n % 5 ? '' : 'Buper') || n;
-    return res === 'SuperBuper' ? (res = 'BOOM') : res;
-  });
-
-const getBombaArr = size => getTemplateArr(size).map(n => checkBomba(n));
-console.table(getBombaArr(10));
-
-//<=====================square left===========================>
-
-const getPerimetrSquareLeft = (size, topRight = []) =>
-  getTemplateArr(size).reduce((pre, cur, i, arr) => {
-    if (i === 0) {
-      topRight = cur.slice(1, -1).reverse();
-    }
-    if (i === arr.length - 1) {
-      return pre.concat(cur, topRight);
-    }
-    pre.push(cur[0]);
-    topRight.unshift(cur.pop());
-    return pre;
-  }, []);
-
-console.log('Square :', getPerimetrSquareLeft(10));
-
-//<=======================square left 2=========================>
-
-const getPerimetrSquare2 = size => {
-  const arr = getTemplateArr(size);
-  let topRight = [];
-  let result = [];
-  arr.forEach((cur, i) => {
-    if (i === 0) {
-      topRight = cur.slice(1, -1).reverse();
-    }
-    result.push(cur[0]);
-    topRight.unshift(cur.pop());
-    if (i === arr.length - 1) {
-      result = result.concat(cur.slice(1), topRight);
-    }
-  });
-  return result;
-};
-console.log('Square2 :', getPerimetrSquare2(10));
-
-//<=====================triangle right ===========================>
-
-const getPerimetrTriangle = (size, diagonal = []) =>
-  getTemplateArr(size).reduce((pre, cur, i, arr) => {
-    if (i === 0) {
-      return pre.concat(cur);
-    }
-    if (i === arr.length - 1) {
-      return pre.concat(cur.pop(), diagonal);
-    }
-    pre.push(cur.pop());
-    diagonal.unshift(cur[i]);
-    return pre;
-  }, []);
-
-console.log('Triangle :', getPerimetrTriangle(10));
-
-//<=======================triangle bottom=========================>
-
-const getPerimetrTriangleBottom = (size, diagonal = []) =>
-  getTemplateArr(size).reduceRight((pre, cur, i, arr) => {
-    if (i === 0) {
-      pre.push(cur[0]);
-      return pre.concat(
-        diagonal,
-        arr
-          .pop()
-          .slice(1, -1)
-          .reverse()
-      );
-    }
-    pre.push(cur[0]);
-    diagonal.unshift(cur[i]);
-    return pre;
-  }, []);
-
-console.log('TriangleBottom :', getPerimetrTriangleBottom(10));
-
-//<=======================helix=========================>
-
-const helix = (input, resultArr, leftSideArr = []) => {
-  if (!input.length) {
-    //end of recursion
-    return resultArr;
+  get forwardStep() {
+    // замыкаем вагоны
+    return this.vagons[++this.vagonCounter % this.vagons.length];
   }
-  //add top side
-  resultArr = resultArr.concat(input.shift());
-  //add right side
-  input.forEach(last => resultArr.push(last.pop()));
-  //create left side
-  input.forEach(first => leftSideArr.unshift(first.shift()));
-  //add bottom side & left side
-  resultArr = input.length
-    ? resultArr.concat(input.pop().reverse(), leftSideArr)
-    : resultArr;
-  //all over again
-  return helix(input, resultArr);
-};
-console.log('helix :', helix(getTemplateArr(10), []));
+  backwordsStep() {
+    return this.vagons[--this.vagonCounter % this.vagons.length];
+  }
+}
+
+function calculateLengthTrain() {
+  // создаём новый поезд
+  const locomotive = new Train(20);
+  console.log('locomotive check', locomotive.vagons.length);
+  let backword = false;
+  let saveCounter = 0;
+  // просыпаемся и включаем свет в первом вагоне
+  const awokeLigth = locomotive.vagons[locomotive.vagonCounter];
+  awokeLigth.lightIsOn = true;
+  // и делаем шаг в перёд
+  let forward = true;
+  locomotive.forwardStep;
+  // до тех пор пока свет горит в первом вогоне
+  while (awokeLigth.lightIsOn) {
+    // идём вперёд
+    while (forward) {
+      let currentStep = locomotive.forwardStep;
+      // если лампочка горит
+      if (currentStep.lightIsOn === true) {
+        //тушим лампочку
+        currentStep.lightIsOn = false;
+        // уходим назад
+        forward = false;
+        backword = true;
+        // сохраняем текущее положение
+        saveCounter = locomotive.vagonCounter;
+      }
+    }
+    // идём назад
+    while (backword) {
+      locomotive.backwordsStep();
+      // доходим до первой лампочки и проверяем горит или нет
+      if (locomotive.vagonCounter === 0) {
+        backword = false;
+        //если лампочка горит идём обратно
+        if (locomotive.vagons[locomotive.vagonCounter].lightIsOn) {
+          forward = true;
+        }
+      }
+    }
+  }
+
+  return saveCounter;
+}
+
+console.log('locomotive length :', calculateLengthTrain());
