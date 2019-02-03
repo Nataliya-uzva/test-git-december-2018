11c9c6dfa67708c77ff13011bf757307371b9d94
Nataliya Uzva <ashatan_u@mail.ru>
2019-02-03 18:25:28 +0200

    genaration table with append and bubbling

--- dom-create-table/app.js
+++ dom-create-table/app.js
@@ -1,39 +1,101 @@
 const page = document.body;
-const div = page.querySelector('div');
+// const div = page.querySelector('[data-view]');
 
 // page.querySelector('button').onclick = generateTable;
 page.querySelector('button').addEventListener('click', generateTable);
 
 function generateTable() {
-    const table = document.createElement('table');
-    const tbody = document.createElement('tbody');
+    // const table = document.createElement('table');
+    // const tbody = document.createElement('tbody');
 
     let count = 0;
 
+    // for (let i = 0; i < 10; i++) {
+    //     const tr = document.createElement('tr');
+    //     for (let j = 0; j < 10; j++) {
+    //         const td = document.createElement('td');
+    //         td.textContent = `${count++ + 1}`;
+    //         td.dataset.value = td.textContent;
+    //         td.addEventListener('click', generateDivText);
+    //         tr.appendChild(td);
+    //     }
+    //     tbody.appendChild(tr);
+    // }
+    let template = '<table><tbody>';
+
     for (let i = 0; i < 10; i++) {
-        const tr = document.createElement('tr');
-        for (let j = 0; j < 10; j++) {
-            const td = document.createElement('td');
-            td.textContent = `${count++ + 1}`;
-            td.dataset.value = td.textContent;
-            td.addEventListener('click', generateDivText);
-            tr.appendChild(td);
-        }
-        tbody.appendChild(tr);
+        template += `
+            <tr>
+                <td data-value='${++count}'>${count}</td>
+                <td data-value='${++count}'>${count}</td>
+                <td data-value='${++count}'>${count}</td>
+                <td data-value='${++count}'>${count}</td>
+                <td data-value='${++count}'>${count}</td>
+                <td data-value='${++count}'>${count}</td>
+                <td data-value='${++count}'>${count}</td>
+                <td data-value='${++count}'>${count}</td>
+                <td data-value='${++count}'>${count}</td>
+                <td data-value='${++count}'>${count}</td>
+            </tr>
+        `;
+
+        // for (let j = 0; j < 10; j++) {
+        //     template += `<td data-value='${count++ + 1}'>
+        //         ${count}
+        //      </td>
+        //     `
+        // }
     }
 
-    table.appendChild(tbody);
-    page.appendChild(table);
+    template += '</tbody></table>';
 
-page.querySelector('button').removeEventListener('click', generateTable);
+    console.log(template);
 
+    page.innerHTML += template;
+
+    // page.querySelectorAll('td')
+    //     .forEach(td => {
+    //         td.addEventListener('click', generateDivText);
+    //     });
+
+    page.querySelector('table').addEventListener('click', (/*{ target }*/ event) => {
+        const target = event.target;
+
+        if (target.tagName !== 'TD') {
+
+            console.log('we are not here!!!');
+            return ;
+        }
+
+        page.querySelector('[data-view]').innerHTML = `${target.dataset.value}`;
+
+        target.textContent = !target.textContent
+            ? target.dataset.value
+            : '';
+
+        // if (target.textContent) {
+        //     target.textContent = '';
+        // } else {
+        //     target.textContent = target.dataset.value;
+        // }
+    });
 }
+
+
+
+
+// console.log(page.querySelectorAll('td'));
+
+
+// page.querySelector('button').removeEventListener('click', generateTable);
+
+// }
 
 
 function generateDivText() {
     const elem = this; // элемент на котором произошло событие
-    
-    div.innerHTML = `${elem.dataset.value}`;
+
+    page.querySelector('[data-view]').innerHTML = `${elem.dataset.value}`;
 
     elem.textContent = !elem.textContent
         ? elem.dataset.value
